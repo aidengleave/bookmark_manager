838f702e16b0fd3c043862096bb5fd0c0ca3cb46
Aiden Gleave <aidengleave@gmail.com>
2019-01-30 18:30:10 +0000

    Wrote tests to build create bookmark functionality

--- bookmark_manager/spec/bookmark_spec.rb
+++ bookmark_manager/spec/bookmark_spec.rb
@@ -15,3 +15,11 @@ describe '.list_all' do
     expect(bookmarks).to include 'http://www.google.com'
   end
 end
+
+describe '.create' do
+  it 'creates a new bookmark' do
+    Bookmark.create(url: 'http://github.com')
+    
+    expect(Bookmark.list_all).to include 'http://github.com'
+  end
+end
