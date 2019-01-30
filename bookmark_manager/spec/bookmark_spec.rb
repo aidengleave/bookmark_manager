e81af0a0e256d90db13ca626800a007dd5544564
Aiden Gleave <aidengleave@gmail.com>
2019-01-30 14:27:20 +0000

    Rewrote feature test to work with database - passing the tests

--- bookmark_manager/spec/bookmark_spec.rb
+++ bookmark_manager/spec/bookmark_spec.rb
@@ -5,9 +5,8 @@ require 'bookmark'
 describe '.list_all' do
   it 'returns a list of bookmarks' do
     bookmarks = Bookmark.list_all
-
-    expect(bookmarks).to include 'http://www.makersacademy.com'
-    expect(bookmarks).to include 'http://www.destroyallsoftware.com'
-    expect(bookmarks).to include 'http://www.google.com'
+    expect(bookmarks).to include "http://www.makersacademy.com"
+    expect(bookmarks).to include "http://www.destroyallsoftware.com"
+    expect(bookmarks).to include "http://www.google.com"
   end
 end
--- bookmark_manager/spec/features/feature_spec.rb
+++ bookmark_manager/spec/features/feature_spec.rb
@@ -5,8 +5,11 @@ feature 'You should see BookmarkManager on the homepage' do
     visit('/')
     expect(page).to have_content 'Bookmark'
   end
-  scenario '/bookmarks exists and has bookmarks' do
+  scenario 'returns a list of bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content("foo.com\nbar.net\nmoo.org")
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
